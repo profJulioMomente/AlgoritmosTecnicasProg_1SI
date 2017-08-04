bcf85ca94e980857432d2451eb20104bd1509392
Julio Cesar Momente <juliocesar.momente@gmail.com>
2017-08-04 19:45:04 -0300

    Ola Mundo
    Código de Olá Mundo escrito em C

---
+++ Aula1/olaMundo.c
@@ -0,0 +1,12 @@
+//Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função Principal
+main() {
+	
+	printf("Ola Mundo!\n"); //Saída de Dados
+	
+	system("pause"); //Comando para pausar o terminal
+	
+}
