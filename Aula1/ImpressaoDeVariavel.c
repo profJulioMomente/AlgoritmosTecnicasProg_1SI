9e7d0797e9843133f0b849b104e342419bcdbe20
Julio Cesar Momente <juliocesar.momente@gmail.com>
2017-08-04 19:59:08 -0300

    Impressão de Valores
    Código para impressão de valores de variáveis

---
+++ Aula1/ImpressaoDeVariavel.c
@@ -0,0 +1,37 @@
+//Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Comentário de várias linhas
+*/
+
+//Função Principal
+main() {
+	int valor_inteiro;
+	float valor_real;
+	double notacao_cientifica;
+	char letra;
+	
+	valor_inteiro = 15;
+	valor_real = 2.375;
+	notacao_cientifica = 1.61e-12;
+	letra = 's';
+	
+	
+	/*
+	 \n - pula linha
+	 Especificadores de formato:
+	 	%d - Inteiros
+	 	%f - Real com ponto flutuante
+	 	%e - Real em notação científica
+	 	%c - Um único caractere
+	 	%s - Palavra ou texto
+	*/
+	printf("Letra: %c \nInteiro: %d \nReal: %f\n", letra, valor_inteiro, valor_real);
+	printf("Notacao cientifica: %e \nPalavra: %s\n\n", notacao_cientifica, "Unilago");
+	
+	
+	system("pause"); //Comando para pausar o terminal
+	
+}
